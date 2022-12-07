e37ea1d0a16f2ffec8987a98aef02ea6620519c1
slitherman <ericcartman2700@gmail.com>
2022-12-07 14:58:35 +0100

    finished the AssignTeachers metod and added a hold navigation property in the underviser model

--- 2. sem projekt boglistesystemet/Models/Underviser.cs
+++ 2. sem projekt boglistesystemet/Models/Underviser.cs
@@ -22,5 +22,6 @@ namespace _2._sem_projekt_boglistesystemet.Models.BookData
         public int Id { get; set; }
         public ICollection<Fag> Fag { get; set; }
         public ICollection<Books> Books { get; set; }
+        public ICollection<Hold> Hold { get; set; }
     }
 }
